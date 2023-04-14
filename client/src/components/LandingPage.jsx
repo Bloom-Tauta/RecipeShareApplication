fca7cc42079b25f8a90227842bc870f6f52e1ce1
+ <+>
2023-04-14 17:18:08 +0300

    Creating LandingPage component

---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./Navbar";
+// import Footer from "./Footer";
+
+
+function LandingPage(){
+    return(
+        <div>
+            <NavBar/>
+            <div className="min-h-screen"></div>
+            {/* <Footer/> */}
+        </div>
+    )
+}
+
+export default LandingPage;
